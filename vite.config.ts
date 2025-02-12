e42c8d0cda85ffd9dac00e643e16cb83e495fbfa
saebyn <saebynx+github@gmail.com>
2025-02-11 17:04:45 -0800

    feat: update package configuration for Vite and remove Rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { dirname, resolve} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   test: {
     environment: "happy-dom",
     setupFiles: ".vitest/setup",
