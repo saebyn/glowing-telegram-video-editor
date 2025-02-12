2170c1c1a8d6e4e8d18c70da828e01fb443789c6
saebyn <saebynx+github@gmail.com>
2025-02-11 17:38:09 -0800

    feat: add vite-plugin-dts for TypeScript declaration file generation and update package version to 0.1.6

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts';
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { dirname, resolve } from 'node:path';
@@ -9,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
