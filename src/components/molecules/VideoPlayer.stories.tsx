8c7060068b2650856cb6983da0c7c1f03cc63488
saebyn <saebynx+github@gmail.com>
2025-03-09 11:19:18 -0700

    show timestamp when moving mouse over timeline Fixes #89

---
+++ src/components/molecules/VideoPlayer.stories.tsx
@@ -0,0 +1,21 @@
+import { action } from "@storybook/addon-actions";
+
+import VideoPlayer  from "./VideoPlayer";
+
+export default {
+  title: "Molecules/VideoPlayer",
+  component: VideoPlayer,
+  tags: ["molecules"],
+};
+
+export const Default = () => {
+  return (
+    <div>
+    <VideoPlayer
+      videoUrl="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      onTimeUpdate={action("Time Update")}
+      onEnd={action("End")}
+    />
+    </div>
+  );
+};
